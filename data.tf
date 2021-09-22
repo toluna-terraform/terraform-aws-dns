e6f47555a7a031bac454aa2b766e0065d262a9f6
ShakedYosef <85097819+ShakedYosef@users.noreply.github.com>
2021-09-22 13:58:14 +0300

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,9 @@
+data "aws_route53_zone" "public" {
+  name         = var.domain
+  private_zone = false
+}
+
+data "aws_acm_certificate" "public" {
+  domain = var.domain
+  types  = ["AMAZON_ISSUED"]
+}
